c6a6a6f3d18bdefe25d5be49d5c4d1f2a7fed0bd
shootinstar000 <93067533+shootinstar000@users.noreply.github.com>
2021-12-01 11:51:41 +0400

    update shorten.py

--- shorten.py
+++ shorten.py
@@ -4,7 +4,7 @@ import sys
 
 def shortener(target_url):
 
-    shrt_url = "http://qps.ru/api?url={tg_url}"
+    shrt_url = "https://da.gd/?url={tg_url}"
 
     req = requests.get(shrt_url.format(tg_url=target_url))
 
