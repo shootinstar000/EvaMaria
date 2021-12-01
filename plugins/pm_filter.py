77f257f460894ebd811a907547f1f2dde29ba0e8
shootinstar000 <93067533+shootinstar000@users.noreply.github.com>
2021-12-01 12:22:39 +0400

    update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -370,11 +370,12 @@ async def cb_handler(client: Client, query: CallbackQuery):
             return await query.answer('No such file exist.')
         files = files_[0]
         title = files.file_name
+        msgid = files.msg_id 
         size=get_size(files.file_size)
         f_caption=files.caption
         if CUSTOM_FILE_CAPTION:
             try:
-                f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
+                f_caption=CUSTOM_FILE_CAPTION.format(file_id=msgid, file_name=title, file_size=size, file_caption=f_caption)
             except Exception as e:
                 logger.exception(e)
             f_caption=f_caption
